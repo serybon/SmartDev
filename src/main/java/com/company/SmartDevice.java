eae8b54cafdd7af1905636fe20f0a4769e8fddf1
Siarhei B <bnds1988@gmail.com>
2025-03-13 17:47:00 +0300

    created turnOn and turnOff methods

--- src/main/java/com/company/SmartDevice.java
+++ src/main/java/com/company/SmartDevice.java
@@ -5,10 +5,19 @@ import java.util.UUID;
 public class SmartDevice {
     private String id;
     private String name;
+    private boolean isOn;
+    private int brightness;
 
     public SmartDevice(String name) {
         this.id = UUID.randomUUID().toString();
         this.name = name;
-
+        this.isOn = false;
     }
-}
+
+    public void turnOn() {
+        isOn = true;
+    }
+
+    public void turnOff() {
+        isOn = false;
+    }
