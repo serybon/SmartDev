398aada9b831f3e96fd0827b3a9fe574310452eb
Siarhei B <bnds1988@gmail.com>
2025-03-13 17:39:18 +0300

    added SmartDevice

---
+++ src/main/java/com/company/SmartDevice.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.UUID;
+
+public class SmartDevice {
+    private String id;
+    private String name;
+
+    public SmartDevice(String name) {
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+    }
+}
