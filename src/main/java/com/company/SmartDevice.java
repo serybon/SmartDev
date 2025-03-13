02fcb4b90069359654a492d2d701e9c4f375ee89
Siarhei B <bnds1988@gmail.com>
2025-03-13 17:52:07 +0300

    added field brightness + setBrightness

--- src/main/java/com/company/SmartDevice.java
+++ src/main/java/com/company/SmartDevice.java
@@ -5,10 +5,16 @@ import java.util.UUID;
 public class SmartDevice {
     private String id;
     private String name;
+    private int brightness;
 
     public SmartDevice(String name) {
         this.id = UUID.randomUUID().toString();
         this.name = name;
-
+        this.brightness = 0;
     }
+
+    public void setBrightness(int brightness) {
+        this.brightness = Math.max(0, Math.min(100, brightness));
+    }
+
 }
